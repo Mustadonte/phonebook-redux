4c3beec1cc767f84b185fa74f2182c30c0f41bb5
Mustadonte <94845588+Mustadonte@users.noreply.github.com>
2022-09-22 17:36:34 +0000

    Deploying to gh-pages from @ Mustadonte/phonebook-redux@a26ad37f4a826a0fad270003fabd907f6cbddd6b 🚀

---
+++ static/js/802.68da5e66.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook_redux=self.webpackChunkphonebook_redux||[]).push([[802],{2802:function(e,n,o){o.r(n),o.d(n,{HomePage:function(){return u}});var h=o(184),u=function(){return(0,h.jsxs)("main",{children:[(0,h.jsx)("h1",{children:"Welcome to my PhoneBook Project"}),(0,h.jsx)("h2",{children:"This is online phonebook which was made bu usint React, Redux and MUI."})]})}}}]);
+//# sourceMappingURL=802.68da5e66.chunk.js.map
