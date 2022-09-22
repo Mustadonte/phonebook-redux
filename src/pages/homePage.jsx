cdbe6468561aae5a87381b1c4183fbb0f1d46022
Ivan Zabolotniy <94845588+Mustadonte@users.noreply.github.com>
2022-09-22 22:15:26 +0300

    Fix text

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,7 +3,7 @@ export const HomePage = () => {
     <main>
       <h1>Welcome to my PhoneBook Project</h1>
       <h2>
-        This is online phonebook which was made bu usint React, Redux and MUI.
+        This is online phonebook which was made by using React, Redux and MUI.
       </h2>
     </main>
   );
